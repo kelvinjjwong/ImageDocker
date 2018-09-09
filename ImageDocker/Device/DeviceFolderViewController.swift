f710edc13034e5dbade2a47b28231f086bdfecee
kelvinjjwong <kelvinjjwong@outlook.com>
2018-09-09 14:57:43 +0800

    add UI component

---
+++ ImageDocker/Device/DeviceFolderViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DeviceFolderViewController.swift
+//  ImageDocker
+//
+//  Created by Kelvin Wong on 2018/9/9.
+//  Copyright © 2018年 nonamecat. All rights reserved.
+//
+
+import Cocoa
+
+class DeviceFolderViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
