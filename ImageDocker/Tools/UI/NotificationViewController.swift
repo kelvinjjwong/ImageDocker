be8bafa0142c8d5ab493041c0fe8e357eafa86e7
kelvinjjwong <kelvinjjwong@outlook.com>
2019-01-10 01:23:30 +0800

    UI adjustment for notification in popover way

---
+++ ImageDocker/Tools/UI/NotificationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationViewController.swift
+//  ImageDocker
+//
+//  Created by Kelvin Wong on 2019/1/9.
+//  Copyright © 2019年 nonamecat. All rights reserved.
+//
+
+import Cocoa
+
+class NotificationViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ ImageDocker/ViewControllerUtils.swift
@@ -0,0 +1,9 @@
+//
+//  ViewControllerUtils.swift
+//  ImageDocker
+//
+//  Created by Kelvin Wong on 2019/1/9.
+//  Copyright © 2019年 nonamecat. All rights reserved.
+//
+
+import Foundation
