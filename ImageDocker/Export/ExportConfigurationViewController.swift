5d3a29ac8fc384d65b4c4767aa41e23bfddaa01a
kelvinjjwong <kelvinjjwong@outlook.com>
2019-09-30 08:23:16 +0800

    add view to configure exporting

---
+++ ImageDocker/Export/ExportConfigurationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ExportConfigurationViewController.swift
+//  ImageDocker
+//
+//  Created by Kelvin Wong on 2019/9/30.
+//  Copyright © 2019 nonamecat. All rights reserved.
+//
+
+import Cocoa
+
+class ExportConfigurationViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
