2d6d2ae679a1beaa0727ea14a09b088e4df1b3cc
kelvinjjwong <kelvinjjwong@outlook.com>
2019-10-01 07:10:51 +0800

    UI adjustment

---
+++ ImageDocker/Export/ExportProfileViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ExportProfileViewController.swift
+//  ImageDocker
+//
+//  Created by Kelvin Wong on 2019/9/30.
+//  Copyright © 2019 nonamecat. All rights reserved.
+//
+
+import Cocoa
+
+
