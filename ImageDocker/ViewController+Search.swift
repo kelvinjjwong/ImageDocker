f573769f32b718e9e09029f524c1b27fcfe345e4
kelvinjjwong <kelvinjjwong@outlook.com>
2021-01-08 02:48:36 +0800

    minor refactor

--- ImageDocker/ViewController+Search.swift
+++ ImageDocker/ViewController+Search.swift
@@ -10,6 +10,22 @@ import Cocoa
 
 extension ViewController {
     
+    func configureMainSearchBar() {
+        self.txtSearch.convertToACBTokenField()
+        self.txtSearch.shouldEnableTokenMenu = true
+        self.txtSearch.shouldDisplaySearchIcon = true
+        self.txtSearch.shouldEnableTokenMenu = true
+        self.txtSearch.tokenDelegate = self
+        self.txtSearch.isEnabled = true
+        self.txtSearch.tokenSeparator = "||"
+        self.txtSearch.target = self
+        self.txtSearch.action = #selector(processMainSearch)
+    }
+    
+    @objc func processMainSearch() {
+        print("======== process main search === \(self.txtSearch.tokenStringValue)")
+    }
+    
     internal func search(_ keyword:String) {
         guard !runningSearch else {
             return
@@ -60,5 +76,84 @@ extension ViewController {
                 self.runningSearch = false
             }
         }
+    }
+}
+
+// shared among all token fields who set delegate to viewController
+extension ViewController: NSTokenFieldDelegate {
+    
+    private func createTokenFieldCompletionMenu(for text:String) -> [String] {
+        if let number = Int(text) {
+            if number >= 1950 && number <= 10000 {
+                return [
+                    "\(text) | Year",
+                    "\(text) | Event",
+                    "\(text) | Place"
+                ]
+            }else if number > 0 && number <= 12 {
+                return [
+                    "\(text) | Month",
+                    "\(text) | Day",
+                    "\(text) | Event",
+                    "\(text) | Place"
+                ]
+            }else if number > 0 && number <= 31 {
+                return [
+                    "\(text) | Day",
+                    "\(text) | Event",
+                    "\(text) | Place"
+                ]
+            }
+        }
+        return [
+            "\(text) | Event",
+            "\(text) | Place",
+            "\(text) | Camera",
+        ]
+    }
+
+    public func tokenField(_ tokenField: NSTokenField, completionsForSubstring substring: String, indexOfToken tokenIndex: Int, indexOfSelectedItem selectedIndex: UnsafeMutablePointer<Int>?) -> [Any]? {
+        
+        return self.createTokenFieldCompletionMenu(for: substring)
+    }
+    
+    public func tokenField(_ tokenField: NSTokenField, menuForRepresentedObject representedObject: Any) -> NSMenu? {
+        print(representedObject)
+        if let token = representedObject as? ACBToken {
+            var substring = token.name
+            if substring.contains("|") {
+                let components = substring.components(separatedBy: "|")
+                substring = components[0].trimmingCharacters(in: .whitespacesAndNewlines)
+            }
+            let nameList = self.createTokenFieldCompletionMenu(for: substring)
+            let menu = NSMenu()
+            nameList.forEach {
+                menu.addItem(withTitle: $0,
+                             action: #selector(tokenFieldMenuItemTapped(_:)),
+                             keyEquivalent: "").target = self
+            }
+            return menu
+        }
+        return nil
+    }
+    
+    @objc private func tokenFieldMenuItemTapped(_ menuItem: NSMenuItem) {
+        if let fieldEditor = txtSearch?.currentEditor() {
+            let textRange = fieldEditor.selectedRange
+            let replaceString = menuItem.title
+            fieldEditor.replaceCharacters(in: textRange, with: replaceString)
+            fieldEditor.selectedRange = NSMakeRange(textRange.location, replaceString.count)
+            txtSearch?.window?.makeFirstResponder(nil)
+            
+        }
+    }
+    
+    public func tokenField(_ tokenField: NSTokenField, styleForRepresentedObject representedObject: Any) -> NSTokenField.TokenStyle {
+        
+        return .rounded
+    }
+
+    public func tokenField(_ tokenField: NSTokenField, hasMenuForRepresentedObject representedObject: Any) -> Bool {
+        return true
     }
 }
