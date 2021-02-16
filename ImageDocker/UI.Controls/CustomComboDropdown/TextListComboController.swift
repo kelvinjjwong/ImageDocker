92fb97740431a6651d9a4973d4196dbd1c40d74b
kelvinjjwong <kelvinjjwong@outlook.com>
2021-02-16 10:47:34 +0800

    re-structure

--- ImageDocker/UI.Controls/CustomComboDropdown/TextListComboController.swift
+++ ImageDocker/UI.Controls/CustomComboDropdown/TextListComboController.swift
@@ -1,9 +1,9 @@
 //
-//  TextListViewController.swift
+//  TextListComboController.swift
 //  ImageDocker
 //
-//  Created by Kelvin Wong on 2019/3/9.
-//  Copyright © 2019年 nonamecat. All rights reserved.
+//  Created by Kelvin Wong on 2021/2/16.
+//  Copyright © 2021 nonamecat. All rights reserved.
 //
 
 import Cocoa
@@ -67,36 +67,4 @@ class TextListComboController : NSObject, NSComboBoxCellDataSource, NSComboBoxDa
     }
     
     
-}
-
-class TextListViewPopupController {
-    
-    var items:[String] = []
-    weak var combobox:NSPopUpButton!
-    
-    init(_ listView:NSPopUpButton){
-        self.combobox = listView
-    }
-    
-    func load(_ items:[String]){
-        self.items = items
-        self.combobox.removeAllItems()
-        self.combobox.addItems(withTitles: items)
-    }
-    
-    func select(_ value:String){
-        self.combobox.selectItem(withTitle: value)
-    }
-    
-    func clean() {
-        self.items = []
-        self.combobox.removeAllItems()
-    }
-    
-    func indexOfSelectedItem() -> Int? {
-        if self.items.count == 0 {
-            return nil
-        }
-        return self.combobox.indexOfSelectedItem
-    }
 }
